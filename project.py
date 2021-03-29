d3584a63dd90638b549ac2f40bb09bf726488f55
Oryngaliyev21 <200101059@stu.sdu.edu.kz>
2021-03-30 00:22:35 +0600

    Made first steps

--- project.py
+++ project.py
@@ -1,1 +1,26 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import streamlit as st
+
+#Application title and basic info
+st.title('COVID-19 pandemic tracker')
+
+
+#Parser block
+url = 'https://www.worldometers.info/coronavirus/'
+req = requests.get(url)
+page = BeautifulSoup(req.content, 'html.parser')
+table = page.find_all('table', id="main_table_countries_today")[0]
+Covid_test1 = pd.read_html(str(table), displayed_only=False)[0]
+
+#Make a pure worksheet without any bugs
+Covid = Covid_test1.drop(Covid_test1.index[[6, 229, 230, 231, 232, 233, 234, 235, 236]])
+Covid['Continent'] = Covid['Continent'].replace([None], 'Cruise ship')
+
+#Creating Sidebars
+st.sidebar.header('User Input Features')
+Selected_Countries = st.sidebar.selectbox('Country', list(Covid['Country,Other']))
+Selected_Continent = st.sidebar.selectbox('Continent', list(set(Covid['Continent'])))
+
 
