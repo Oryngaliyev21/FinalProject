dc2b16c6c80bdf211f880dd01216286858915589
Nurtileu <200101053@stu.sdu.edu.kz>
2021-03-31 22:48:34 +0500

    Divided original dataframe to Continent and Countries (dfs). Inserted new sidebar and made modification of existed sidebars.

--- project.py
+++ project.py
@@ -6,7 +6,6 @@ import streamlit as st
 #Application title and basic info
 st.title('COVID-19 pandemic tracker')
 
-
 #Parser block
 url = 'https://www.worldometers.info/coronavirus/'
 req = requests.get(url)
@@ -18,10 +17,14 @@ Covid_test1 = pd.read_html(str(table), displayed_only=False)[0]
 Covid = Covid_test1.drop(Covid_test1.index[[6, 229, 230, 231, 232, 233, 234, 235, 236]])
 Covid['Continent'] = Covid['Continent'].replace([None], 'Cruise ship')
 Covid = Covid.drop('#', 1)
+Covid = Covid.fillna(0)
+Covid_Continents = Covid.drop(Covid.index[7:])
+Covid_Countries = Covid.drop(Covid.index[0:7])
 
 #Creating Sidebars
 st.sidebar.header('User Input Features')
-Selected_Countries = st.sidebar.selectbox('Country', list(sorted(Covid['Country,Other'])))
-Selected_Continent = st.sidebar.selectbox('Continent', list(sorted(set(Covid['Continent']))))
+Selected_Continent = st.sidebar.multiselect('Continent', list(sorted(set(Covid_Countries['Continent']))))
+Selected_Countries = st.sidebar.multiselect('Country', list(sorted(Covid_Countries['Country,Other'][8:])))
+Selected_Attributes = st.sidebar.multiselect('Attribute', list(Covid_Countries.columns[1:]))
 
 
